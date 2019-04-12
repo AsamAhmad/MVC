ab4b6901db3617370a29180b3113037f252a3070
@AsamAhmad <asam93700@gmail.com>
2019-04-12 11:32:43 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+echo '<h1>  Hello World </h1> 
+
+<style>
+h1
+{
+    font-size: 20em;
+    
+    color: red;
+}
+</style> ';
+
+
+
+
+# Aperçu
+var_dump($_GET);
+
+# Récupération des paramètres GET
+$controller = $_GET['controller'];
+$action = $_GET['action'];
+
+
+
+
+
